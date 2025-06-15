1cb197a73cfada8c27de8a32e6041fe6f7b81fa4
sirokuma0108 <sirokuma1045@gmail.com>
2025-06-16 02:49:18 +0900

    コミットメッセージ

--- public/script.js
+++ public/script.js
@@ -4,6 +4,13 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
 
+// 接続時にユーザー名を聞く
+let userName = "";
+while (!userName) {
+  userName = prompt("ユーザー名を入力してください");
+}
+socket.emit("set name", userName);
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
