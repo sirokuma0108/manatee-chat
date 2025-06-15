9316ac9a5891ef759c5161651f0c8be467a5066b
sirokuma0108 <sirokuma1045@gmail.com>
2025-06-16 03:14:56 +0900

    コミットメッセージ

--- public/script.js
+++ public/script.js
@@ -9,28 +9,20 @@ while (!userName) {
   userName = prompt("ユーザー名を入力してください");
 }
 
-console.log("ユーザー名送信:", userName);
 socket.emit("set name", userName);
-
-// 入室メッセージを受けてチャットフォーム表示
-form.style.display = "none";
 
 socket.on("chat message", (msg) => {
   const li = document.createElement("li");
   li.textContent = msg;
   messages.appendChild(li);
   messages.scrollTop = messages.scrollHeight;
-
-  if (msg.includes(`ようこそ、${userName}さん`)) {
-    form.style.display = "block";
-    input.focus();
-  }
 });
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit("chat message", input.value);
+  e.preventDefault(); // ページリロードを防ぐ
+  const message = input.value.trim();
+  if (message) {
+    socket.emit("chat message", message);
     input.value = "";
   }
 });
